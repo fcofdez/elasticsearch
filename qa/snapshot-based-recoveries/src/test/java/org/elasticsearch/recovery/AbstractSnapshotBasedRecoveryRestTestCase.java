a696609f5ee66efc6c125f36f8bae8aa54f35e49
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2021-08-12 13:49:05 +0200

    Add integration tests for recover from snapshot

--- qa/snapshot-based-recoveries/src/test/java/org/elasticsearch/recovery/AbstractSnapshotBasedRecoveryRestTestCase.java
+++ qa/snapshot-based-recoveries/src/test/java/org/elasticsearch/recovery/AbstractSnapshotBasedRecoveryRestTestCase.java
@@ -8,6 +8,7 @@
 
 package org.elasticsearch.recovery;
 
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
@@ -24,10 +25,12 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 
 public abstract class AbstractSnapshotBasedRecoveryRestTestCase extends ESRestTestCase {
@@ -57,9 +60,9 @@ public abstract class AbstractSnapshotBasedRecoveryRestTestCase extends ESRestTe
         );
         ensureGreen(indexName);
 
-        final int numDocs = randomIntBetween(1, 500);
+        final int numDocs = randomIntBetween(500, 1000);
         indexDocs(indexName, numDocs);
-
+        waitUntilGlobalCheckpointIsStable(indexName);
         forceMerge(indexName, randomBoolean(), randomBoolean());
 
         deleteSnapshot(REPOSITORY_NAME, SNAPSHOT_NAME, true);
@@ -69,27 +72,47 @@ public abstract class AbstractSnapshotBasedRecoveryRestTestCase extends ESRestTe
         updateIndexSettings(indexName, Settings.builder().put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 1));
         ensureGreen(indexName);
 
-        for (int i = 0; i < 4; i++) {
-            assertSearchResultsAreCorrect(indexName, numDocs);
-        }
+        assertMatchAllReturnsAllDocuments(indexName, numDocs);
+        assertMatchQueryReturnsAllDocuments(indexName, numDocs);
+
         deleteSnapshot(REPOSITORY_NAME, SNAPSHOT_NAME, false);
     }
 
-    private void assertSearchResultsAreCorrect(String indexName, int numDocs) throws IOException {
-        if (randomBoolean()) {
-            Map<String, Object> searchResults = search(indexName, QueryBuilders.matchAllQuery());
-            assertThat(extractValue(searchResults, "hits.total.value"), equalTo(numDocs));
-            List<Map<String, Object>> hits = extractValue(searchResults, "hits.hits");
-            for (Map<String, Object> hit : hits) {
-                String docId = extractValue(hit, "_id");
-                assertThat(Integer.parseInt(docId), allOf(greaterThanOrEqualTo(0), lessThan(numDocs)));
-                assertThat(extractValue(hit, "_source.field"), equalTo(Integer.parseInt(docId)));
-                assertThat(extractValue(hit, "_source.text"), equalTo("Some text " + docId));
+    private void waitUntilGlobalCheckpointIsStable(String index) throws Exception {
+        assertBusy(() -> {
+            final Request request = new Request(HttpGet.METHOD_NAME, '/' + index + "/_stats?level=shards");
+            final Response response = client().performRequest(request);
+            assertOK(response);
+            Map<String, Object> responseAsMap = responseAsMap(response);
+            Map<String, Object> indices = extractValue(responseAsMap, "indices");
+            Map<String, Object> indexShardsStats = extractValue(extractValue(indices, index), "shards");
+            List<Map<String, Object>> shardStats = extractValue(indexShardsStats, "0");
+            for (Map<String, Object> shardStat : shardStats) {
+                final boolean isPrimary = extractValue(shardStat, "routing.primary");
+                if (isPrimary == false) {
+                    continue;
+                }
+                Map<Object, Integer> seqNos = extractValue(shardStat, "seq_no");
+                assertThat(seqNos.toString(), seqNos.get("max_seq_no"), is(equalTo(seqNos.get("global_checkpoint"))));
             }
-        } else {
-            Map<String, Object> searchResults = search(indexName, QueryBuilders.matchQuery("text", "some"));
-            assertThat(extractValue(searchResults, "hits.total.value"), equalTo(numDocs));
+        }, 60, TimeUnit.SECONDS);
+    }
+
+    private void assertMatchAllReturnsAllDocuments(String indexName, int numDocs) throws IOException {
+        Map<String, Object> searchResults = search(indexName, QueryBuilders.matchAllQuery());
+        assertThat(extractValue(searchResults, "hits.total.value"), equalTo(numDocs));
+        List<Map<String, Object>> hits = extractValue(searchResults, "hits.hits");
+        for (Map<String, Object> hit : hits) {
+            String docId = extractValue(hit, "_id");
+            assertThat(Integer.parseInt(docId), allOf(greaterThanOrEqualTo(0), lessThan(numDocs)));
+            assertThat(extractValue(hit, "_source.field"), equalTo(Integer.parseInt(docId)));
+            assertThat(extractValue(hit, "_source.text"), equalTo("Some text " + docId));
         }
+    }
+
+    private void assertMatchQueryReturnsAllDocuments(String indexName, int numDocs) throws IOException {
+        Map<String, Object> searchResults = search(indexName, QueryBuilders.matchQuery("text", "some"));
+        assertThat(extractValue(searchResults, "hits.total.value"), equalTo(numDocs));
     }
 
     private static void forceMerge(String index, boolean onlyExpungeDeletes, boolean flush) throws IOException {
