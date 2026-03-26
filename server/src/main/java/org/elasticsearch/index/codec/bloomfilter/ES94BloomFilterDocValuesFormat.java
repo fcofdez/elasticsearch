75d9d846cb4e8aaadea796385e9ad6706e8ea9ef
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2026-03-26 15:33:49 +0100

    Num hash

--- server/src/main/java/org/elasticsearch/index/codec/bloomfilter/ES94BloomFilterDocValuesFormat.java
+++ server/src/main/java/org/elasticsearch/index/codec/bloomfilter/ES94BloomFilterDocValuesFormat.java
@@ -97,7 +97,7 @@ public class ES94BloomFilterDocValuesFormat extends DocValuesFormat {
 
     // We use prime numbers with the Kirsch-Mitzenmacher technique to obtain multiple hashes from two hash functions
     private static final int[] PRIMES = new int[] { 2, 5, 11, 17, 23, 29, 41, 47, 53, 59, 71 };
-    private static final int DEFAULT_NUM_HASH_FUNCTIONS = 7;
+    private static final int DEFAULT_NUM_HASH_FUNCTIONS = 4;
     // With the default oversize factor of 24 and 7 hash functions, the theoretical false positive rate is approximately 6.63E-5,
     // calculated as (1 - e^(-k/o))^k where k = number of hash functions and o = oversize factor.
     //
